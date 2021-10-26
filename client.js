b363ec5267511ec7b6f7e0d6f3272bd480481ee5
rafaelem2 <62569684+rafaelem2@users.noreply.github.com>
2021-10-25 23:21:28 -0300

    7.5 feito

--- client.js
+++ client.js
@@ -18,6 +18,12 @@ function jacaWars(v, theta) {
   }
 }
 
+function somaMaior(numeros){
+  numeros.sort(function(x,y){return x-y}) //uso do stackoverflow para sort
+  return (numeros[1] + numeros[numeros.length-2])
+	
+}
+
 function anoBissexto(n) {
 	if ((n % 4 == 0 && n % 100 != 0) || n % 400 == 0) {
 		return true;
@@ -25,6 +31,52 @@ function anoBissexto(n) {
 	return false;
 }
 
+
+function primo(num) {
+  for (var i = 2; i < num; i++)
+    if (num % i === 0)
+      return false;
+  return num > 1;
+}
+
+//stackoverflow
+function enesimo(n) {
+  let cont = 0
+  let i =0
+  while (true) {
+      if (primo(i) === true){
+          cont++
+      }
+      if (cont === n) {
+          return i;
+      }
+      i++
+  };
+}
+
+function checkPalindrome(str) {     
+  for(var i = 0; i < str.length / 2; i++) if (str[i] != str[str.length - i - 1]) return false;
+  return true;
+}
+
+function contaPalindromo(str){
+  let cont = 0
+  for(var i = 0; i < str.length; i++) if (checkPalindrome(str[i]) === true) cont ++;
+  return cont
+}
+
+function somaValores(objetos){
+  var soma = 0;
+  for (chave in objetos){
+    soma += objetos[chave] 
+  }
+
+  return soma
+}
+function somaInts(string){
+  const sumall = string.reduce((a, b) => Number(a) + Number(b)); //https://www.delftstack.com/howto/javascript/sum-array-of-objects-javascript/
+  return(sumall);
+}
 
 axios({
   method: 'post',
@@ -41,16 +93,44 @@ axios({
     .then((response)=>{
       const soma = response.data.soma.entrada.a + response.data.soma.entrada.b
       console.log(soma)
+      axios.post('https://tecweb-js.insper-comp.com.br/exercicio/soma', {resposta:soma},options2);
       const tamanho = response.data['tamanho-string'].entrada.string.length
       console.log(tamanho)
+      axios.post('https://tecweb-js.insper-comp.com.br/exercicio/tamanho-string', {resposta:tamanho},options2);
       const usuario = response.data['nome-do-usuario'].entrada.email.split("@")[0]
       console.log(usuario)
+      axios.post('https://tecweb-js.insper-comp.com.br/exercicio/nome-do-usuario', {resposta:usuario},options2);
       const jw = jacaWars(response.data['jaca-wars'].entrada.v,response.data['jaca-wars'].entrada.theta)
       console.log(jw)
+      axios.post('https://tecweb-js.insper-comp.com.br/exercicio/jaca-wars', {resposta:jw},options2);
       const bissexto = anoBissexto(response.data['ano-bissexto'].entrada.ano)
       console.log(bissexto)
-      const volumePizza = Math.round(Math.PI * ((response.data['volume-da-pizza'].entrada.z)^2) * response.data['volume-da-pizza'].entrada.a);
+      axios.post('https://tecweb-js.insper-comp.com.br/exercicio/ano-bissexto', {resposta:bissexto},options2);
+      const volumePizza = Math.round(Math.PI * ((response.data['volume-da-pizza'].entrada.z)*(response.data['volume-da-pizza'].entrada.z)) * response.data['volume-da-pizza'].entrada.a);
       console.log(volumePizza)
+      axios.post('https://tecweb-js.insper-comp.com.br/exercicio/volume-da-pizza', {resposta:volumePizza},options2);
+      const mru = response.data.mru.entrada.s0 + response.data.mru.entrada.v * response.data.mru.entrada.t 
+      console.log(mru)
+      axios.post('https://tecweb-js.insper-comp.com.br/exercicio/mru', {resposta:mru},options2);
+      const inverte = response.data['inverte-string'].entrada.string.split('').reverse().join('')
+      console.log(response.data['inverte-string'].entrada.string)
+      console.log(inverte)
+      axios.post('https://tecweb-js.insper-comp.com.br/exercicio/inverte-string', {resposta:inverte},options2);
+      const nPrimo = enesimo(response.data['n-esimo-primo'].entrada.n)
+      console.log(nPrimo)
+      console.log(response.data['n-esimo-primo'].entrada.n)
+      axios.post('https://tecweb-js.insper-comp.com.br/exercicio/n-esimo-primo', {resposta:nPrimo},options2);
+      const soma2 = somaMaior(response.data['soma-segundo-maior-e-menor-numeros'].entrada.numeros);
+      console.log(soma2)
+      axios.post('https://tecweb-js.insper-comp.com.br/exercicio/soma-segundo-maior-e-menor-numeros', {resposta:soma2},options2);
+      const palindromo = contaPalindromo(response.data['conta-palindromos'].entrada.palavras)
+      console.log(palindromo)
+      axios.post('https://tecweb-js.insper-comp.com.br/exercicio/conta-palindromos', {resposta:palindromo},options2);
+      const valores = somaValores(response.data['soma-valores'].entrada.objeto)
+      axios.post('https://tecweb-js.insper-comp.com.br/exercicio/soma-valores', {resposta:valores},options2);
+      const somaStrings = somaInts(response.data['soma-de-strings-de-ints'].entrada.strings)
+      axios.post('https://tecweb-js.insper-comp.com.br/exercicio/soma-de-strings-de-ints', {resposta:somaStrings},options2);
+
     })
     
 
